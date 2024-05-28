9fc243ba04ae0a73848e24af87340f038963293b
mayoreee <mayoreee1@gmail.com>
2024-05-28 23:12:42 +0200

    feat: map

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,15 +1,67 @@
-"use client"
+"use client";
+import { useState, useEffect } from "react";
 import Footer from "@/components/footer";
 import { NavBar } from "@/components/nav-bar";
 import useMerchants from "@/hooks/use-merchants";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+const defaultLocation: [number, number] = [37.7749, -122.4194]; // San Francisco coordinates
 
 export default function Map() {
   const { merchants, error } = useMerchants();
+  const [userLocation, setUserLocation] = useState<[number, number] | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation([
+            position.coords.latitude,
+            position.coords.longitude,
+          ]);
+        },
+        (error) => {
+          console.error("Error getting user's location: ", error);
+          setUserLocation(defaultLocation); // Default to San Francisco if location access is denied
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+      setUserLocation(defaultLocation); // Default to San Francisco if Geolocation is not supported
+    }
+  }, []);
+
   return (
-    <div>
-      <NavBar merchants={merchants} />
-      <main className="flex flex-col"></main>
-      <Footer />
+    <div className="relative">
+      {userLocation && (
+        <div
+          id="map"
+          className="fixed inset-0 z-[-1]"
+          style={{ height: "100vh", width: "100vw" }}
+        >
+          <MapContainer
+            center={userLocation}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={userLocation}>
+              <Popup>You are here.</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      )}
+      <div className="relative z-10">
+        <NavBar merchants={merchants} />
+        <Footer />
+      </div>
     </div>
   );
 }
