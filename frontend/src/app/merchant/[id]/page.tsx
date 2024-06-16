7541969050cfc76cf991c083f36fd849d51d1491
mayoreee <mayoreee1@gmail.com>
2024-06-16 13:57:32 +0200

    feat: get merchat info

--- frontend/src/app/merchant/[id]/page.tsx
+++ frontend/src/app/merchant/[id]/page.tsx
@@ -2,19 +2,21 @@
 import Footer from "@/components/footer";
 import { NavBar } from "@/components/nav-bar";
 import useMerchants from "@/hooks/use-merchants";
-import { usePathname } from 'next/navigation';
-
+import { useParams } from "next/navigation";
 
 export default function MerchantPage() {
   const { merchants, error } = useMerchants();
-  const pathname = usePathname();
+  const { id } = useParams();
+
+  const merchant = merchants.find(
+    (merchant: any) => (merchant.merchantId === id)
+  );
 
   return (
     <div>
       <NavBar merchants={merchants} />
       <main className="flex flex-col"></main>
       <Footer />
-      <div className="justify-center"><h1>{JSON.stringify(merchants)}</h1></div>
     </div>
   );
 }
