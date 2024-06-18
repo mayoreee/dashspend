bb1159ada97954b0db941ba5a49373bf6a2eef7d
mayoreee <mayoreee1@gmail.com>
2024-06-18 19:57:16 +0200

    add docker file

--- backend/clustering-service/tests/stub.ts
+++ backend/clustering-service/tests/stub.ts
@@ -8,7 +8,11 @@ function createPgPool(): Pool {
     const password = String(process.env.PG_PASSWORD)
     const database = String(process.env.PG_DATABASE)
     const port = Number(process.env.PG_PORT)
-    const db = new Postgress(host, username, password, database, port)
+    const ssl = {
+        rejectUnauthorized: true,
+        ca: String(process.env.PG_SSL_CA),
+      };
+    const db = new Postgress(host, username, password, database, port, ssl)
     return db.getPool()
 }
 
